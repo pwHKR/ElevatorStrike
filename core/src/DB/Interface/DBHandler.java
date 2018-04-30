d6400261c01562c2fce23b2759dc4f5e16f6527e
Stefan Smudja <stefan.smudja0001@stud.hkr.se>
2018-04-30 13:36:49 +0200

    Påbörjat databas implementation

--- core/src/DB/Interface/DBHandler.java
+++ core/src/DB/Interface/DBHandler.java
@@ -1,10 +1,25 @@
 package DB.Interface;
 
+import com.sun.tools.javac.comp.Todo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by woojen on 2018-04-20.
  */
 
 public class DBHandler implements Multiplayer {
+
+
+    // TODO: 2018-04-30 Implementera databas 
+
+    public DBHandler() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://206.189.50.69:3306/Elevator?autoReconnect=true&useSSL=false&" +
+                "user=stefan&password=kameler1337");
+    }
+
 
     @Override
     public void setPlayerPosition(float x, float y) {
