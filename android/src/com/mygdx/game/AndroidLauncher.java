0333eafd882945cf6f0ea3e763e7264b2fd55b6f
andreasp1997 <andreas.petersson0312@stud.hkr.se>
2018-05-24 11:02:14 +0200

    Brightness adjust for battery percentage

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,8 +1,13 @@
 package com.mygdx.game;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
+import android.os.BatteryManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -17,9 +22,61 @@ import java.sql.SQLException;
 
 
 public class AndroidLauncher extends AndroidApplication {
+
+
+	//Variable for brightness value
+	private int brightness;
+	//Content resolver used as handle to the system's settings
+	private ContentResolver contentResolver;
+	//Window object that stores a reference to the current window
+	private Window window;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		//Brightness adaption for battery level
+
+		IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+		Intent batteryStatus = getContext().registerReceiver(null, iFilter);
+
+		//Check if phone is charging
+		int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+		boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+				status == BatteryManager.BATTERY_STATUS_FULL;
+
+		int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+		float batteryPct = level / (float)scale;
+
+		Log.d("Battery: ", String.valueOf(batteryPct));
+
+		contentResolver = getContentResolver();
+
+		//Get the current window
+
+		window = getWindow();
+
+		try {
+			// To handle the auto
+			Settings.System.putInt(contentResolver,
+					Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+			//Get the current system brightness
+			brightness = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);
+		}
+		catch (Settings.SettingNotFoundException e) {
+			Log.e("Error", "Cannot access system brightness");
+			e.printStackTrace();
+		}
+
+		//Set the system brightness in if statement below
+		if(batteryPct < 0.5 || !isCharging){
+			Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, 125);
+		}
+
+		//Test
+		//Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, 40);
 
 
 		try {
